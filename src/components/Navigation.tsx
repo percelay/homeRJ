cf6712d60ae3644eace3939e74f5720fef9af451
percelay <dalepercelay@gmail.com>
2026-02-16 21:06:44 -0500

    logo tek og

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,11 +22,11 @@ export default function Navigation() {
       <div className="mx-auto max-w-7xl px-6 flex items-center justify-between h-20">
         <Link href="/" className="flex items-center gap-3">
           <Image
-            src="/images/Group 46.png"
+            src="/images/general/logo.png"
             alt="RJE Home Remodeling"
-            width={270}
-            height={90}
-            className="h-18 w-auto"
+            width={180}
+            height={60}
+            className="h-12 w-auto"
             priority
           />
         </Link>
